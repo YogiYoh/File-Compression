8befda0905d1a2692d428374b3f72a63a91dca24
BuildTools <unconfigured@null.spigotmc.org>
2024-05-20 16:11:57 -0700

    Made User Input for character list

--- File Compression/fileComp.c
+++ File Compression/fileComp.c
@@ -8,9 +8,23 @@ int main(){
     printf("How many numbers do you want to input?: "); 
 
     scanf("%d", &input); 
-
+    getchar(); 
     // Debugging input 
-    printf("\nThe numbers of inputs is: %d", input); 
+    printf("The numbers of inputs is: %d", input); 
 
+    char character[input]; 
+
+
+    for(int i = 0; i < input; i++){
+        printf("Give me a character: "); 
+        scanf("%c", &c); 
+        character[i] = c; 
+    }
+
+    for(int i = 0; i < input; i++){
+        printf("\n%c", character[i]); 
+    }
+
+    return 0; 
 
 }
