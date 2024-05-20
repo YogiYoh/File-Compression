22f5af0eab7b6c4c23ecdb925ec97a1dccad63b2
BuildTools <unconfigured@null.spigotmc.org>
2024-05-20 15:50:42 -0700

    Made User Input

--- File Compression/fileComp.c
+++ File Compression/fileComp.c
@@ -1,7 +1,16 @@
 #include <stdio.h> 
+#include <stdlib.h> 
 
 int main(){
+    int input; 
+    int x; 
+    char c; 
+    printf("How many numbers do you want to input?: "); 
 
-printf("Hello World!");
+    scanf("%d", &input); 
+
+    // Debugging input 
+    printf("\nThe numbers of inputs is: %d", input); 
+
 
 }
