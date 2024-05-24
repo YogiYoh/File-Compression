5003bc947191c4b3cc245c4b144dab58a7384828
BuildTools <unconfigured@null.spigotmc.org>
2024-05-23 19:28:57 -0700

    Created an Insert function for BST

--- File Compression/BinSearch.c
+++ File Compression/BinSearch.c
@@ -8,16 +8,29 @@ typedef struct Node{
 
 }BstNode; 
 
-BstNode createNode(int data){
+BstNode* createNode(int data){
     BstNode* newNode = (BstNode*)malloc(sizeof(BstNode)); 
     newNode->data = data; 
     newNode->left = newNode->right = NULL; 
+    return newNode; 
+}
+
+
+BstNode* Insert(BstNode* root, int data){
+    if(root == NULL){
+        root = createNode(data); 
+    }else if(data <= root->data){
+        root->left = Insert(root->left, data);
+    }else{
+        root->right = Insert(root->right, data);
+    }
+    return root;
 }
 
 
 int main(){
     BstNode* root  = NULL; // Store address of root Node 
-    Insert(root, 15); 
-    Insert(root, 10); 
-    Insert(root, 20); 
+    root = Insert(root, 15); 
+    root = Insert(root, 10); 
+    root = Insert(root, 20); 
 }
