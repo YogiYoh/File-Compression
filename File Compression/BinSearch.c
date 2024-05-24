9db4338639c8b5bc48a9544b3bcd4b3ceaeaa8c7
BuildTools <unconfigured@null.spigotmc.org>
2024-05-23 21:32:52 -0700

    Created a Search Function for BST

--- File Compression/BinSearch.c
+++ File Compression/BinSearch.c
@@ -1,6 +1,6 @@
 #include <stdlib.h> 
 #include <stdio.h> 
-
+#include <stdbool.h> 
 
 typedef struct Node{
     int data; 
@@ -27,10 +27,30 @@ BstNode* Insert(BstNode* root, int data){
     return root;
 }
 
+bool Search(BstNode* root, int data){
+    if(root == NULL){
+        return false;
+    }else if(root->data == data){
+        return true; 
+    }else if(data <= root->data){
+        return Search(root->left, data); 
+    }else{
+        return Search(root->right, data); 
+    }
+
+    return false;
+}
 
 int main(){
     BstNode* root  = NULL; // Store address of root Node 
     root = Insert(root, 15); 
     root = Insert(root, 10); 
     root = Insert(root, 20); 
+    root = Insert(root, 15); 
+
+    int value = 20;
+    bool found = Search(root, value);
+    printf("\n%d is %s in the BST.\n", value, found ? "found" : "not found");
+
+
 }
