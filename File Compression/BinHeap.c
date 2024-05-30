820344bbcd37a47e3f031209a8e0e11bf390fd0a
BuildTools <unconfigured@null.spigotmc.org>
2024-05-29 19:06:44 -0700

    Renamed createNode function to defaultNode for clarity Added capacity parameter to the new createNode function to allow specific capacity Implemented a resize function

--- File Compression/BinHeap.c
+++ File Compression/BinHeap.c
@@ -12,7 +12,7 @@ typedef struct Node{
 
 }BinHeap;
 
-BinHeap* createNode(){
+BinHeap* defaultNode(){
     BinHeap* node = (BinHeap*)(malloc)(sizeof(BinHeap)); 
     node->value = (char*)(malloc)(DEFAULT_CAPACITY * sizeof(char)); 
 
@@ -22,3 +22,32 @@ BinHeap* createNode(){
     return node; 
 }
 
+BinHeap* createNode(int capacity){
+    BinHeap* node = (BinHeap*)(malloc)(sizeof(BinHeap)); 
+    node->value = (char*)(malloc)(capacity * sizeof(char)); 
+
+    node->capacity = capacity; 
+    node->size = 0;
+
+    return node; 
+}
+
+void resize(BinHeap* node, int new_capacity){
+    if(node->capacity < new_capacity){
+        char* temp = (char*)(malloc)(new_capacity * sizeof(char)); 
+        for(int i = 0; i < node->capacity; i++){
+            temp[i] = node->value[i]; 
+        }
+
+        char* old = node->value; 
+        node->value = temp;
+        node->capacity = new_capacity; 
+        free(old);
+    }
+}
+
+int main(){
+
+
+    
+}
