c1a0b9d85a088882400756024886fc72b3fc2459
BuildTools <unconfigured@null.spigotmc.org>
2024-05-30 21:14:56 -0700

    Modified Some Functions/Created Swim Function Modified all the getLeft/Right Child functions and also changed getParentNode to handle indexing

--- File Compression/BinHeap.c
+++ File Compression/BinHeap.c
@@ -6,7 +6,7 @@
 
 
 typedef struct Node{
-    char* value; 
+    int* value; 
     int size; 
     int capacity;
 
@@ -14,7 +14,7 @@ typedef struct Node{
 
 BinHeap* defaultNode(){
     BinHeap* node = (BinHeap*)(malloc)(sizeof(BinHeap)); 
-    node->value = (char*)(malloc)(DEFAULT_CAPACITY * sizeof(char)); 
+    node->value = (int*)(malloc)(DEFAULT_CAPACITY * sizeof(int)); 
 
     node->capacity = DEFAULT_CAPACITY; 
     node->size = 0;
@@ -24,7 +24,7 @@ BinHeap* defaultNode(){
 
 BinHeap* createNode(int capacity){
     BinHeap* node = (BinHeap*)(malloc)(sizeof(BinHeap)); 
-    node->value = (char*)(malloc)(capacity * sizeof(char)); 
+    node->value = (int*)(malloc)(capacity * sizeof(int)); 
 
     node->capacity = capacity; 
     node->size = 0;
@@ -34,57 +34,78 @@ BinHeap* createNode(int capacity){
 
 void resize(BinHeap* node, int new_capacity){
     if(node->capacity < new_capacity){
-        char* temp = (char*)(malloc)(new_capacity * sizeof(char)); 
+        int* temp = (int*)(malloc)(new_capacity * sizeof(int)); 
         for(int i = 0; i < node->capacity; i++){
             temp[i] = node->value[i]; 
         }
 
-        char* old = node->value; 
+        int* old = node->value; 
         node->value = temp;
         node->capacity = new_capacity; 
         free(old);
     }
 }
 
-char findMax(char* value){
+int findMax(int* value){
     return value[1]; 
 }
 
-char getLeftChild(int index, BinHeap* node){
-    char leftChild = node->value[2 * index]; 
+int getLeftChild(int index, BinHeap* node){
+    int leftChild = node->value[2 * index]; 
 
     if(2 * index < node->size){
         return leftChild; 
     }
+
+    return -1; 
 }
 
-char getParentNode(int index, char* value){
+int getParentNode(int index, int* value){
     int parent = index / 2; 
     if(parent != 0){
         return value[parent]; 
     }else{
         return findMax(value); 
     }
+
+    return -1; 
 }
 
 
 
-char getRightChild(int index, BinHeap* node){
-    char leftChild = node->value[(2 * index) + 1]; 
+int getRightChild(int index, BinHeap* node){
+    int leftChild = node->value[(2 * index) + 1]; 
 
     if((2 * index) + 1 < node->size){
         return leftChild; 
     }
+
+    return -1; 
 }
 
-BinHeap* swim(BinHeap* node){
-    if(node->value[node->size] > node->value[node->size - 1]){
-        
+void swim(int size, BinHeap* node){
+
+    if(size <= 0){
+        return; 
     }
+
+    int parentNode = (size - 1) / 2; 
+
+    if(node->value[size] <= node->value[parentNode]){
+        return; 
+    }
+
+    int value = node->value[parentNode]; 
+
+    node->value[parentNode] = node->value[size]; 
+
+    node->value[size] = value; 
+    swim(parentNode, node); 
 }
 
 
 BinHeap* insert(BinHeap* node, int value){
+    
     if(node->size > node->capacity){
         fprintf(stderr, "Error: Heap Size is greater than Heap Capacity!\n");
         return node; 
@@ -95,7 +116,7 @@ BinHeap* insert(BinHeap* node, int value){
         
     node->value[node->size] = value; 
     node->size = node->size + 1; 
-    swim(node); 
+    swim(node->size - 1, node); 
     return node; 
 }
 
@@ -124,7 +145,13 @@ int main(){
     BinHeap* buh = createNode(6); 
 
     insert(buh, 10); 
+    printf("Size: %d\n", buh->size); 
+    printBin(buh);
     insert(buh, 9);
+    printf("Size: %d\n", buh->size); 
+    printBin(buh);
+    insert(buh, 30);
+    printf("Size: %d\n", buh->size); 
     printBin(buh); 
 
 
