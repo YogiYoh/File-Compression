d3084f2ff39fbdd643f0ee1af73bc13a5416131c
BuildTools <unconfigured@null.spigotmc.org>
2024-06-13 21:06:22 -0700

    Created HuffMan struct and created "createHuff" function for Huffman encoding
    Defined HuffMan struct with frequency, characters, and uniqueChars variable
    Made createHuff function to initialize HuffMan struct with dynamic memory allocation

--- File Compression/simpHuff.c
+++ File Compression/simpHuff.c
@@ -3,6 +3,26 @@
 #include <ctype.h>
 #include "MinHeap.c"
 
+typedef struct Node{
+    int* frequency; 
+    char* characters;
+    int uniqueChars; 
+}HuffMan;
+
+
+HuffMan* createHuff(int capacity){
+    HuffMan* node = (HuffMan*)(malloc)(sizeof(HuffMan)); 
+        
+    node->frequency = (int*)(malloc)(sizeof(int) * capacity); 
+
+    node->characters = (char*)(malloc)(sizeof(char) * capacity); 
+    node->uniqueChars = 0; 
+    return node; 
+}
+
+
+
+
     void checkFrequency(char c[], int input)
     {
         int frequency[26] = {0}; // 26 is the amount of char in alphabet 
